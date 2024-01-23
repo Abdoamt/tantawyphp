e1446eb3f86b2d23ab288679dff6ac999b0aafb1
Abdoamt <143970915+Abdoamt@users.noreply.github.com>
2024-01-24 00:40:07 +0200

    Add files via upload

---
+++ lockwebsite/index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>AMT - Lock Website</title>
+        <link rel="icon" href="https://icons.iconarchive.com/icons/hopstarter/soft-scraps/16/Lock-Lock-icon.png" width="16" height="16">
+        <link rel="icon" href="Hopstarter-Soft-Scraps-Lock-Lock.16.png">
+        <style>
+            body{
+    background-color:darkred;
+}
+h1{
+    color:yellow;
+    font-size: 40px;
+    width:340px;
+    text-align: center;
+    border-radius: 20px;
+    border:solid yellow;
+}
+.a{
+    height:45px;
+    font-weight: bold;
+    width:320px;
+    font-size:30px;
+    text-align: center;
+    color:black;
+    background-color: yellow;
+    border-radius: 25px;
+    border:solid red;
+}
+.p{
+    height:45px;
+    font-weight: bold;
+    width:300px;
+    font-size:30px;
+    text-align: center;
+    color:black;
+    background-color: yellow;
+    border-radius: 25px;
+    border:solid red;
+}
+.c{
+    height:45px;
+    font-weight: bold;
+    font-size:30px;
+    width:145px;
+    text-align: center;
+    color:black;
+    background-color: yellow;
+    border-radius: 25px;
+    border:solid red;
+}
+.c:hover{
+    height:45px;
+    width:145px;
+    font-weight: bold;
+    text-align: center;
+    color:black;
+    font-size:30px;
+    background-color: green;
+    border-radius: 25px;
+    border:solid blue;
+}
+.d{
+    height:45px;
+    width:145px;
+    font-weight: bold;
+    text-align: center;
+    color:black;
+    background-color: yellow;
+    border-radius: 25px;
+    font-size:30px;
+    border:solid red;
+}
+.d:hover{
+    height:45px;
+    width:145px;
+    font-size:30px;
+    font-weight: bold;
+    text-align: center;
+    color:black;
+    background-color: red;
+    border-radius: 25px;
+    border:solid blue;
+}
+form{
+    background-color: black;
+    width:370px;
+    height:540px;
+    border-radius: 25px;
+    border:solid yellow;
+    background-position: center;
+}
+.e{
+    height:55px;
+    width:55px;
+    border-radius: 50%;
+    background-position: center;
+}
+p{
+    color:yellow;
+    text-align: center;
+    font-weight: bold;
+    font-style: italic;
+    font-size:20px;
+}
+@media(min-width:350px) and (max-width:600px){
+    body{
+        background-color:darkblue;
+    }
+    h1{
+        color:yellow;
+        font-size: 30px;
+        width:280px;
+        text-align: center;
+        border-radius: 20px;
+        border:solid yellow;
+    }
+    .a{
+        width:280px;
+        height:40px;
+        font-size:20px;
+        color:black;
+        background-color: yellow;
+        text-align: center;
+        border-radius: 20px;
+        border:solid darkblue;
+    }
+    .p{
+        width:280px;
+        height:40px;
+        font-size:20px;
+        color:black;
+        background-color: yellow;
+        text-align: center;
+        border-radius: 20px;
+        border:solid darkblue;
+    }
+    form{
+        background-color: green;
+        width:310px;
+        height:490px;
+        border-radius: 25px;
+        border:solid darkblue;
+        background-position: center;
+    }
+    .c{
+        height:40px;
+        font-weight: bold;
+        font-size:25px;
+        width:140px;
+        text-align: center;
+        color:black;
+        background-color: yellow;
+        border-radius: 25px;
+        border:solid green;
+    }
+    .d{
+        height:40px;
+        font-weight: bold;
+        font-size:25px;
+        width:140px;
+        text-align: center;
+        color:black;
+        background-color: yellow;
+        border-radius: 25px;
+        border:solid red;
+    }
+}
+        </style>
+    </head>
+    <meta name="google-site-verification" content="QvkmKylaDyhcCnW4WmRwCXFCF5h1cGH0sBOCR1C6ygc" />    
+    <body>
+        <?php
+        $a=$_GET['one'];
+        $p=$_GET['two'];
+        $c=$_GET['three'];
+        $d=$_GET['four'];
+if($a=="abdulrahman888") and ($p=="787898"){
+    header('location:http://softwarecodes.66ghz.com/index/');
+}
+else{
+    header('location:http://softwarecodes.66ghz.com/tryagain/');
+}
+?>
+        <center>
+        <form method="GET" action="https://computerworld-website8.on.drv.tw/personalwebsite/">
+            <h1>قم بادخال معلوماتك</h1>
+            <p>Abdelrahman Tantawy</p>
+            <p>Private Website</p>
+            <img src="Dakirby309-Simply-Styled-OS-Windows.256.png" alt="" class="e"></br></br>
+            <input type="text" class="a" name="one" placeholder="E-mail Or Username"></br></br>
+            <input type="password" class="p" name="two" placeholder="Enter Password"></br></br>
+            <input type="submit" value="Login" class="c" name="three"></br></br>
+            <input type="reset" value="Delete" class="d" name="four">
+        </form>
+    </center>
+    </body>
+</html> 
